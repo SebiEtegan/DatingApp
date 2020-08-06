ff5ecfeb8ebf5539b9f1c2a408247565145348ee
SebiEtegan <etegansebi@gmail.com>
2020-08-06 16:57:17 +0300

    End of section 16

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -33,6 +33,8 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -57,7 +59,8 @@ export class TimeAgoExtendsPipe extends TimeAgoPipe {}
       MemberDetailComponent,
       MemberEditComponent,
       PhotoEditorComponent,
-      TimeAgoExtendsPipe
+      TimeAgoExtendsPipe,
+      MemberMessagesComponent,
    ],
    imports: [
       BrowserModule,
@@ -90,6 +93,7 @@ export class TimeAgoExtendsPipe extends TimeAgoPipe {}
       MemberEditResolver,
       PreventUnsavedChanges,
       ListsResolver,
+      MessagesResolver,
    ],
    bootstrap: [
       AppComponent
